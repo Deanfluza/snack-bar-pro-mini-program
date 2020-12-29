92963d0ee360f8d4cd1d9b2b10b70166ea2cbde1
panzihua <774629027@qq.com>
2020-12-29 17:27:47 +0800

    feat: order history page front-end

--- src/api/order.js
+++ src/api/order.js
@@ -6,3 +6,14 @@ exports.createOrder = (order) => {
   const url = `${perfix}/orders`
   return axios.post(url, order)
 }
+
+exports.getAllOrders = () => {
+  const url = `${perfix}/orders`
+  return axios.get(url)
+}
+
+exports.updateOrder = (order) => {
+  const url = `${perfix}/orders`
+  order['_id'] = order.orderNumber
+  return axios.put(url, order)
+}
